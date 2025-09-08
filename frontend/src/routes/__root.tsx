63f21382d963dca3bd8c26f3f3a26c256e4d5c7f
biprotosh <biprotosh123@gmail.com>
2025-09-08 11:50:52 +0530

    changed the way of importing

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,9 +1,7 @@
 import { Outlet, createRootRoute } from '@tanstack/react-router'
 import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
 import { TanstackDevtools } from '@tanstack/react-devtools'
-import Navbar from '@/components/Navbar'
-
-
+import Navbar from '../components/Navbar'
 
 export const Route = createRootRoute({
     component: () => (
